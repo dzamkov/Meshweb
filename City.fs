584a26b00c518cbb5caa44e9cc2452a7ac423661
dzamkov <dzamkov@xiatek.org>
2012-02-04 21:54:55 -0500

    Better city generator

--- City.fs
+++ City.fs
@@ -14,17 +14,14 @@ type CityParameters = {
         /// The extent of the city away from the origin.
         Extent : float<m>
 
-        /// The maximum area of a plot in the city.
-        MaximumPlotArea : float<m^2>
+        /// The average area of a plot in the city.
+        PlotArea : float<m^2>
 
         /// A value that indicates how biased plots are to occur near the city center.
         CenterBias : float
 
         /// A value that indicates how biased plots are to occur in groups.
         GroupBias : float
-
-        /// A value that indicates how biased plots are to occur at all.
-        PopulationBias : float
     }
 
 /// Generates a city using the given parameters. A collection of points representing the locations
@@ -32,42 +29,46 @@ type CityParameters = {
 let generate (parameters : CityParameters) (random : Random) =
     let accum = List<Point> ()
     let extent = float parameters.Extent
-    let totalCityArea = 4.0 * extent * extent
-    let plotArea = float parameters.MaximumPlotArea
+    let cityArea = 4.0 * extent * extent
+    let plotArea = float parameters.PlotArea
     let centerBias = parameters.CenterBias
     let groupBias = parameters.GroupBias
-    let populationBias = parameters.PopulationBias
 
-    // Generates plots for the given area.
-    let rec generatePlots (accum : List<Point>) (potential : float) (area : Rectangle) =
-        let size = area.Size
-        let totalArea = area.Area
-        let center = area.Center
+    // Generates plots for the given zone.
+    let rec generateZone (accum : List<Point>) (potential : float) (zone : Rectangle) =
+        let size = zone.Size
+        let area = zone.Area
+        let center = zone.Center
 
         // Modify population potential.
         let distance = center.Length
         let relativeDistance = distance / extent
-        let relativeArea = totalArea / totalCityArea
+        let relativeArea = area / cityArea
         let plotGroupBias = Math.Pow (relativeArea, groupBias)
         let potential = potential + (random.NextDouble () - 0.5) * plotGroupBias
 
-        if totalArea < plotArea then
-            if potential + populationBias + (1.0 - relativeDistance * 2.0) * centerBias >= 0.0  then
-                accum.Add area.Center
+        if area < plotArea * 1.5 then
+            if potential + (1.0 - relativeDistance * 2.0) * centerBias >= 0.0  then
+                accum.Add center
         else
+
+            // Determine road width.
+            let roadWidth = 5.0 * (area ** 0.1)
+            let roadWidth = if roadWidth > 5.0 then roadWidth else 0.0
+
             let aspect = size.X / size.Y
             let split = Math.Asin (random.NextDouble () * 0.5 - 0.5) / Math.PI + 0.5
             if random.NextDouble () * (1.0 + aspect) < aspect then
-                let split = area.Min.X + size.X * split
-                generatePlots accum potential (Rectangle (area.Min.X, area.Min.Y, split, area.Max.Y))
-                generatePlots accum potential (Rectangle (split, area.Min.Y, area.Max.X, area.Max.Y))
+                let split = zone.Min.X + size.X * split
+                generateZone accum potential (Rectangle (zone.Min.X, zone.Min.Y, split - roadWidth, zone.Max.Y))
+                generateZone accum potential (Rectangle (split + roadWidth, zone.Min.Y, zone.Max.X, zone.Max.Y))
             else
-                let split = area.Min.Y + size.Y * split
-                generatePlots accum potential (Rectangle (area.Min.X, area.Min.Y, area.Max.X, split))
-                generatePlots accum potential (Rectangle (area.Min.X, split, area.Max.X, area.Max.Y))
+                let split = zone.Min.Y + size.Y * split
+                generateZone accum potential (Rectangle (zone.Min.X, zone.Min.Y, zone.Max.X, split - roadWidth))
+                generateZone accum potential (Rectangle (zone.Min.X, split + roadWidth, zone.Max.X, zone.Max.Y))
 
     // Generate plots for the whole city.
-    generatePlots accum 0.0 (Rectangle (-extent, -extent, extent, extent))
+    generateZone accum 0.0 (Rectangle (-extent, -extent, extent, extent))
 
     // Return the locations of the plots.
     accum :> seq<Point>
--- Program.fs
+++ Program.fs
@@ -10,14 +10,13 @@ open Meshweb.Render
 
 let random = Random 1337
 let cityParameters = {
-        Extent = 4800.0<m>
-        MaximumPlotArea = 800.0<m^2> * 2.0
-        CenterBias = 1.2
-        GroupBias = 0.05
-        PopulationBias = 0.0
+        Extent = 14800.0<m>
+        PlotArea = 800.0<m^2>
+        CenterBias = 2.5
+        GroupBias = 0.02
     }
-for i = 0 to 20 do
+for i = 0 to 5 do
     let points = generate cityParameters random
     let viewExtent = (float cityParameters.Extent) * 1.2
-    let bitmap = renderPoints (Size (1024, 1024)) (Rectangle (-viewExtent, -viewExtent, viewExtent, viewExtent)) points
+    let bitmap = renderPoints (Size (2048, 2048)) (Rectangle (-viewExtent, -viewExtent, viewExtent, viewExtent)) points
     bitmap.Save (String.Format ("test{0}.png", i))
