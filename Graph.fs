39ed17ee2cab8e0868e8726beb902133f67b9e16
dzamkov <dzamkov@xiatek.org>
2012-02-05 16:49:37 -0500

    Graphs and color-coded debug images

---
+++ Graph.fs
@@ -0,0 +1,32 @@
+﻿module Meshweb.Graph
+
+open System
+open System.Collections.Generic
+
+/// An undirected graph with vertices of a certain type.
+type Graph<'a when 'a : equality> (vertices : seq<'a * 'a[]>) =
+    let connectionMap = dict vertices
+
+    /// Gets the readonly connection map for this graph.
+    member this.ConnectionMap = connectionMap
+
+    /// Gets a collection of all vertices in this graph.
+    member this.Vertices = connectionMap.Keys :> seq<'a>
+
+    /// Gets the neighbors for the given vertex.
+    member this.GetNeighbors vertex = connectionMap.[vertex]
+
+/// Creates a graph with the given vertices and neighbors.
+let createFromNeighbors vertices = Graph<'a> vertices
+
+/// Creates a graph with the given vertices and edges. Each edge should only
+/// appear once.
+let createFromEdges vertices edges =
+    let connections = Dictionary<'a, List<'a>> ()
+    for vertex in vertices do
+        connections.[vertex] <- List<'a> ()
+    for edge in edges do
+        let a, b = edge
+        connections.[a].Add b
+        connections.[b].Add a
+    Graph<'a> (connections |> Seq.map (fun kvp -> kvp.Key, kvp.Value.ToArray ()))
--- Program.fs
+++ Program.fs
@@ -4,6 +4,7 @@ open System
 open System.Collections.Generic
 open System.Drawing
 
+open Meshweb.Graph
 open Meshweb.Geometry
 open Meshweb.City
 open Meshweb.Render
@@ -17,8 +18,18 @@ let cityParameters = {
     }
 for i = 0 to 20 do
     let points = generate cityParameters random
-    let neighbors = findNeighbors 200.0 (Seq.zip (Seq.initInfinite id) points)
+    let pointRefs = Seq.map ref points
+    let neighbors = findNeighbors 200.0 (Seq.map (fun ref -> ref, !ref) pointRefs)
+    let graph = createFromEdges pointRefs neighbors
+
+    let coloredPoints = graph.ConnectionMap |> Seq.map (fun kvp ->
+            let degree = kvp.Value.Length
+            let color = Color.FromArgb (255, 255, max (255 - degree * 9) 0, max (255 - degree * 5) 0)
+            let position = !kvp.Key
+            position, color
+        )
+
     let viewExtent = (float cityParameters.Extent) * 1.2
     let view = (Rectangle (-viewExtent, -viewExtent, viewExtent, viewExtent))
-    let bitmap = renderPointsMonochrome (Color.Black) (Color.White) (Size (1024, 1024)) view points
+    let bitmap = renderPoints (Color.Black) (Size (1024, 1024)) view coloredPoints
     bitmap.Save (String.Format ("test{0}.png", i))
